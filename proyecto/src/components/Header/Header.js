1c916e1f08e9dafb67bca5a3ef5982140969115b
Mora Baylac <morabaylac@MacBook-Pro-de-Mora.local>
2021-09-02 09:45:00 -0400

    Header

---
+++ proyecto/src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React, {Component} from React;
+
+class Header extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <header>
+                <h1>Titulo/Nombre de App</h1>
+                <section>
+                    <p>Ordenar ASC/ DESC</p>
+                    <i className="fas fa-th"></i>
+                    <i className="fas fa-align-justify"></i>
+                    <form action="">
+                        <input input type="text" name="search" id="" placeholder="Search"></input>
+                        <button type="submit"><i className="fas fa-search"></i></button>
+                    </form>
+                </section>
+            </header>
+        )
+    }
+}
+
+export default Header
