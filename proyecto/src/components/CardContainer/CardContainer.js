1c6714787f57d2db5cb9bae7f2cd7c7e978d1dfe
Lola Marinelli <lolamarinelli@Lolas-MacBook-Pro.local>
2021-09-06 11:23:55 -0300

    fetch

--- proyecto/src/components/CardContainer/CardContainer.js
+++ proyecto/src/components/CardContainer/CardContainer.js
@@ -5,17 +5,36 @@ class CardContainer extends Component{
     constructor(){
         super()
         this.state = {
+            canciones: [],
+            isLoaded: false,
+            /* nextUrl: '' */
 
         }
     }
+
+    componentDidMount(){ //Aqui adentro traemos la informacion (de la API)
+        
+        console.log('monte Canciones')
+
+        let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0';
+        console.log(url)
+       
+        fetch(url)
+        .then( response => response.json())
+        .then( data => {
+            console.log(data);
+            this.setState({
+                canciones: data.data,
+                isLoaded: true,
+            })
+        })
+        .catch( e => console.log(e))
+
+    }
+
     render(){
         return(
             <section className="card-container">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
                 <Card />
             </section>
         )
