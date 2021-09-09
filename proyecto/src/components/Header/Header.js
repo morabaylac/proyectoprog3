ff8e94844fa36e2a895fef9e0bcbf3fbd7b7a4ee
lolamarinelli <lmarinelli@udesa.edu.ar>
2021-09-09 10:44:58 -0300

    gif y css

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -10,7 +10,7 @@ class Header extends Component{
     render(){
         return(
             <header>
-                <h1>Play</h1>
+                <img src="/assets/images/logo.png" class="logo-header uk-animation-scale-up" alt="logo" />
                 <section>
                     <i className="fas fa-th"></i>
                     <i className="fas fa-align-justify"></i>
